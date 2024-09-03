2b0dd1f5722234dfdbd65b3f781dbfc83185490e
MAugusto89 <souzadagama966.masdg@gmail.com>
2024-09-03 19:23:10 -0400

    Primeiro Commit

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+    cy.get('[type="text"]').type(description);
+    cy.get('[type="date"]').type(deadline);
+    
   })
 })
