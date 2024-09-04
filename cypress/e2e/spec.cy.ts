76b3faab5473e39e8cee6c71598bd264b9fdb410
MAugusto89 <souzadagama966.masdg@gmail.com>
2024-09-03 20:22:24 -0400

    corringo testes

--- cypress/e2e/spec.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-    cy.get('[type="text"]').type(description);
-    cy.get('[type="date"]').type(deadline);
-    
-  })
-})
